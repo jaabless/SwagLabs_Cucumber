a1a15e64a6b8777573f96bc44caf083527884470
jaabless <jefferyasamoahbless@gmail.com>
2025-08-28 22:21:10 +0000

    test(base): modify BaseTest to support cross browser testing in headless

--- src/test/java/com/swaglabs/config/BaseTest.java
+++ src/test/java/com/swaglabs/config/BaseTest.java
@@ -4,27 +4,46 @@ import com.swaglabs.pages.LoginPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 public class BaseTest {
     private static WebDriver driver;
     protected static LoginPage loginPage;
 
-
-
     public static WebDriver getDriver() {
         if (driver == null) {
-            WebDriverManager.chromedriver().setup();
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless=new"); // headless mode
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            WebDriverManager.chromedriver().setup();
+            String env = System.getProperty("env", "local");   // local or docker
+            String browser = System.getProperty("browser", "chrome"); // chrome or edge
+            boolean headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
+
+                // --- Run locally ---
+                if (browser.equalsIgnoreCase("chrome")) {
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--headless=new"); // headless mode
+                    options.addArguments("--no-sandbox");
+                    options.addArguments("--disable-dev-shm-usage");
 //            driver = new ChromeDriver();
-            driver = new ChromeDriver(options); //headless
-            driver.manage().window().maximize();
+                    driver = new ChromeDriver(options); //headless
+                    driver.manage().window().maximize();
+
+                } else if (browser.equalsIgnoreCase("edge")) {
+                    System.setProperty("webdriver.edge.driver", "resources/msedgedriver.exe");
+                    driver = new EdgeDriver();
+                    driver.manage().window().maximize();
+
+                } else {
+                    throw new RuntimeException("Unsupported browser: " + browser);
+                }
+
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         }
         return driver;
     }
